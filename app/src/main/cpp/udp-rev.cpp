1f1ec379dc4504977aa1418b1db55384c5a5c3c8
Marc <zunly@hotmail.com>
2022-08-01 15:23:46 +0800

    开启一个循环运行的线程.

--- app/src/main/cpp/udp-rev.cpp
+++ app/src/main/cpp/udp-rev.cpp
@@ -2,6 +2,8 @@
 #include <cstring>
 #include <android/log.h>
 #include <unistd.h>
+#include <cstdio>
+#include <__threading_support>
 
 #define TAG "UDP-LIB"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__);
@@ -11,8 +13,73 @@ struct Jni_Context {
     jobject instance = nullptr;
 };
 
+JavaVM *jvm = nullptr;
+bool ifSubThreadRunning = true;
+
+jint JNI_OnLoad(JavaVM *vm, void *args) {
+    ::jvm = vm;
+    return JNI_VERSION_1_6;
+}
+
+void *sub_keep_running_thread(void *args) {
+
+    // step 1 get args
+    struct Jni_Context *jniContext = static_cast<Jni_Context *>(args);
+
+    // step 2 get jniEnv
+    JNIEnv *subThreadEnv = nullptr;
+    jint attachResult = ::jvm->AttachCurrentThread(&subThreadEnv, nullptr);
+
+    if (attachResult != JNI_OK) {
+        LOGD("Attach sub thread jni env failed \n");
+        return nullptr;
+    }
+
+    // step 3 get jclass/jobj
+    jobject job = jniContext->instance;
+    if (job == nullptr) {
+        LOGD("No jobject passed to here \n");
+    }
+
+    jclass clazz = subThreadEnv->GetObjectClass(job);
+
+    jmethodID jmId = subThreadEnv->GetMethodID(clazz, "changeText", "(Ljava/lang/String;)V");
+
+    // jfieldID textValFid = subThreadEnv->GetFieldID(clazz, "text2", "Ljava/lang/String;");
+    int counter = 0;
+    char str[20];
+
+    while (ifSubThreadRunning) {
+
+        sprintf(str, "%d", counter);
+        LOGD("%d", counter);
+        jstring newVal = subThreadEnv->NewStringUTF(str);
+        subThreadEnv->CallVoidMethod(job, jmId, newVal);
+        // subThreadEnv->SetObjectField(job, textValFid, newVal);
+        counter++;
+        sleep(1);
+    }
+
+    return nullptr;
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_wulala_myapplicationktjni_MainActivity_stringFromJNI(JNIEnv *env, jobject) {
     jstring hw = env->NewStringUTF("Hello from C++");
     return hw;
+}
+
+struct Jni_Context jniContext;
+pthread_t pid;
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_wulala_myapplicationktjni_MainActivity_changeTextThreadStart(JNIEnv *env, jobject thiz) {
+
+    jniContext.jniEnv = env;  // 这句是没有意义的.
+    jniContext.instance = env->NewGlobalRef(thiz);   // 提升为全局引用
+
+    pthread_create(&pid, nullptr, sub_keep_running_thread, (void *) &jniContext);
+
+    // pthread_join(pid, nullptr);
 }
--- app/src/main/java/com/wulala/myapplicationktjni/MainActivity.kt
+++ app/src/main/java/com/wulala/myapplicationktjni/MainActivity.kt
@@ -1,35 +1,59 @@
 package com.wulala.myapplicationktjni
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import com.wulala.myapplicationktjni.ui.theme.MyApplicationKTJniTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val TAG = MainActivity.javaClass.simpleName
     companion object {
         init {
             System.loadLibrary("udprev")
         }
     }
 
+    var textVal = stringFromJNI()
+    //var text2 = String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val textVal = stringFromJNI()
         setContent {
+
+            var text2 by remember { mutableStateOf("abcd") }
+
             MyApplicationKTJniTheme {
                 Scaffold() {
                     Column() {
                         PlayerScreen(textVal)
+
+                        Text(text2)
+
+                        Button(onClick = {
+                            changeTextThreadStart()
+                            changeText("start")
+                        }) {
+                            Text("开启C子线程")
+                            //
+                        }
                     }
                 }
             }
         }
     }
 
+    private fun changeText(newVal: String) {
+        Log.d(TAG, "changeText: " + newVal)
+    }
+
+    private external fun changeTextThreadStart()
     private external fun stringFromJNI(): String
 
 }
